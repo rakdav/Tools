3bbbac0ac98cdfdfcbee43f45e9ba62d528ce0d2
rakdav <rakdav@inbox.ru>
2020-09-28 12:36:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tools/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.tools;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
