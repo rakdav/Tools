0a593706d2b3fac44a67e2295235aac3c745d5fa
rakdav <rakdav@inbox.ru>
2020-10-12 11:45:26 +0200

    16

--- app/src/main/java/com/example/tools/CellAnimateActivity.java
+++ app/src/main/java/com/example/tools/CellAnimateActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.Image;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class CellAnimateActivity extends AppCompatActivity {
@@ -12,5 +14,12 @@ public class CellAnimateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cell_animate);
+        ImageView img = (ImageView)findViewById(R.id.rabitAnim);
+        // определим для ImageView какое-нибудь изображение
+        img.setImageResource(R.drawable.multfilm);
+        // создаем анимацию
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.losash_animation);
+        // запуск анимации
+        img.startAnimation(animation);
     }
 }
