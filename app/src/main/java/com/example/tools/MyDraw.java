3210695b3db706e6b794d7b34438e7cfbf4c07f7
rakdav <rakdav@inbox.ru>
2020-10-12 11:07:17 +0200

    14

--- app/src/main/java/com/example/tools/MyDraw.java
+++ app/src/main/java/com/example/tools/MyDraw.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 public class MyDraw extends View {
@@ -23,5 +24,11 @@ public class MyDraw extends View {
         canvas.drawCircle(getWidth()/2,getHeight()/2,50,paint);
         paint.setColor(Color.GREEN);
         canvas.drawRect(10,10,30,30,paint);
+        String str="Fumishin terrorist";
+        Rect rect=new Rect();
+        paint.getTextBounds(str,0,str.length(),rect);
+        canvas.rotate(-45,rect.exactCenterX(),rect.exactCenterY());
+
+        canvas.drawText(str,50,250,paint);
     }
 }
